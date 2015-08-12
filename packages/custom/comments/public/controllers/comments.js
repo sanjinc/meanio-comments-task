f9b1fd8719e67e1bcf30494814925af78bfa2f8e
Sanjin Celeski <sanjin.celeski@gmail.com>
2015-08-13 01:58:14 +0200

    make user data available inside $scope

--- packages/custom/comments/public/controllers/comments.js
+++ packages/custom/comments/public/controllers/comments.js
@@ -1,11 +1,13 @@
 'use strict';
 
 /* jshint -W098 */
-angular.module('mean.comments').controller('CommentsController', ['$scope', 'Global', 'Comments',
-  function($scope, Global, Comments) {
+angular.module('mean.comments').controller('CommentsController', ['$scope', 'Global', 'Comments', 'MeanUser',
+  function($scope, Global, Comments, MeanUser) {
     $scope.global = Global;
     $scope.package = {
       name: 'comments'
     };
+    $scope.user = MeanUser.user;
+    $scope.isAdmin = MeanUser.isAdmin;
   }
 ]);
