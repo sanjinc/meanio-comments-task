f77d3b60763071ef17fc95aeab8b897a1b87d958
Sanjin Celeski <sanjin.celeski@gmail.com>
2015-08-13 01:45:10 +0200

    bootstrap server side "comments" controller

---
+++ packages/custom/comments/server/controllers/comments.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+    Comment = mongoose.model('Comment'),
+    _ = require('lodash');
+
+module.exports = function(Comments) {
+
+    return {
+
+    };
+}
