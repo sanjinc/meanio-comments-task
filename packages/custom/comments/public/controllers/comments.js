d33a916b896f34a3273a42832a649dd95d069c6e
Sanjin Celeski <sanjin.celeski@gmail.com>
2015-08-13 02:08:24 +0200

    add comments create functionality

--- packages/custom/comments/public/controllers/comments.js
+++ packages/custom/comments/public/controllers/comments.js
@@ -1,14 +1,33 @@
 'use strict';
 
 /* jshint -W098 */
-angular.module('mean.comments').controller('CommentsController', ['$scope', 'Global', 'Comments', 'MeanUser',
-  function($scope, Global, Comments, MeanUser) {
+angular.module('mean.comments').controller('CommentsController', ['$scope', 'Global', 'Comments', 'MeanUser', '$stateParams',
+  function($scope, Global, Comments, MeanUser, $stateParams) {
     $scope.global = Global;
     $scope.package = {
       name: 'comments'
     };
     $scope.user = MeanUser.user;
     $scope.isAdmin = MeanUser.isAdmin;
+    $scope.create = function(isValid) {
+      if (isValid) {
+        var comment = new Comments({
+          user: $scope.user.name,
+          parent: $stateParams.articleId,
+          body: $scope.comment.body
+        });
+        comment.$save({
+          articleId: $stateParams.articleId
+        }, function(response) {
+          if($scope.isAdmin) {
+            $scope.comments.unshift(response);
+          }
+        });
+        $scope.comment.body = "";
+      } else {
+        $scope.submitted = true;
+      }
+    };
     $scope.findComments = function(article) {
       $scope.comments = [];
       Comments.query({
